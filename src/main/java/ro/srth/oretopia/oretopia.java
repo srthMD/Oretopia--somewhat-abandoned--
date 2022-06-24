e9bf916305a9424dd705cc4312ed5785d3c31556
srthMD <mq3g.mq5g@gmail.com>
2022-06-24 17:46:47 -0400

    optimize imports

--- src/main/java/ro/srth/oretopia/oretopia.java
+++ src/main/java/ro/srth/oretopia/oretopia.java
@@ -1,30 +1,19 @@
 package ro.srth.oretopia;
+
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IItemProvider;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ro.srth.oretopia.block.ModBlocks;
 import ro.srth.oretopia.item.ModItems;
 import ro.srth.oretopia.util.ModSoundEvents;
-/* im sorry can i have the kids back now */
-import java.util.stream.Collectors;
 
 @Mod(oretopia.MOD_ID)
 public class oretopia
